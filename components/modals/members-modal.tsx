8a58af3090d8b4a35f56494c3b6f0f352ba71a11
carloslin <carloslin@phalanxbiotech.com>
2023-12-19 18:17:15 +0800

    members-modal add dropdownMenu

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { ShieldCheck, ShieldAlert } from "lucide-react";
-import { UserAvatar } from "@/components/user-avatar";
 import { useState } from "react";
+import {
+  ShieldCheck,
+  ShieldAlert,
+  MoreVertical,
+  ShieldQuestion,
+  Shield,
+  Check,
+  Gavel,
+} from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -10,9 +17,21 @@ import {
   DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const roleIconMap = {
   GUEST: null,
@@ -50,7 +69,45 @@ export const MembersModal = () => {
                 <p className="text-xs text-zinc-500">{member.profile.email}</p>
               </div>
               {server.profileId !== member.profileId &&
-                loadingId !== member.id && <div>Actions</div>}
+                loadingId !== member.id && (
+                  <div className="ml-auto">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreVertical className="h-4 w-4 text-zinc-500" />
+                        <DropdownMenuContent side="left">
+                          <DropdownMenuSub>
+                            <DropdownMenuSubTrigger className="flex items-center">
+                              <ShieldQuestion className="w-4 h-4 mr-2" />
+                              <span>Role</span>
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                              <DropdownMenuSubContent>
+                                <DropdownMenuItem>
+                                  <Shield className="h-4 w-4 mr-2" /> Guest
+                                  {member.role === "GUEST" && (
+                                    <Check className="h-4 w-4 ml-auto" />
+                                  )}
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                  <ShieldCheck className="h-4 w-4 mr-2" />
+                                  Moderator
+                                  {member.role === "MODERATOR" && (
+                                    <Check className="h-4 w-4 ml-auto" />
+                                  )}
+                                </DropdownMenuItem>
+                              </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                          </DropdownMenuSub>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>
+                            <Gavel className="h-4 w-4 mr-2" />
+                            Kick
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenuTrigger>
+                    </DropdownMenu>
+                  </div>
+                )}
             </div>
           ))}
         </ScrollArea>
