9976afef4c42122613fb8bc5ba24e7658acc47d6
carloslin <carloslin@phalanxbiotech.com>
2023-12-27 18:02:59 +0800

    server-section add

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,11 +1,13 @@
+import { Hash, Mic, Video, ShieldCheck, ShieldAlert } from "lucide-react";
 import { ChannelType, MemberRole } from "@prisma/client";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { ServerHeader } from "@/components/server/server-header";
 import { ServerSearch } from "@/components/server/server-search";
-import { Hash, Mic, Video, ShieldCheck, ShieldAlert } from "lucide-react";
+import { ServerSection } from "@/components/server/server-section";
+import { currentProfile } from "@/lib/current-profile";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -120,6 +122,17 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
