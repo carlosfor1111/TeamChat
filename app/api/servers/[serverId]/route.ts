712f0be01c659d6224e422843c8a5c37a7a8f88d
carloslin <carloslin@phalanxbiotech.com>
2024-01-12 11:40:51 +0800

    socket io add

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,6 +1,8 @@
+import { NextResponse } from "next/server";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+
 export async function DELETE(
   req: Request,
   { params }: { params: { serverId: string } }
