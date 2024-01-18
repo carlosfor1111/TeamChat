7857356566fd78778a6aff5463e4c735442b9ae7
carloslin <carloslin@phalanxbiotech.com>
2024-01-18 17:44:11 +0800

    io

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -19,6 +19,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
       // @ts-ignore
       addTrailingSlash: false,
     });
+    //io
     res.socket.server.io = io;
   }
 
