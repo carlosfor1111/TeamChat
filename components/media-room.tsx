58992522908e6347f4ab11707ea6362387a8791d
carloslin <carloslin@phalanxbiotech.com>
2024-01-22 16:17:25 +0800

    media room

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 
@@ -10,13 +11,9 @@ interface MediaRoomProps {
   chatId: string;
   video: boolean;
   audio: boolean;
-};
+}
 
-export const MediaRoom = ({
-  chatId,
-  video,
-  audio
-}: MediaRoomProps) => {
+export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
   const { user } = useUser();
   const [token, setToken] = useState("");
 
@@ -27,26 +24,24 @@ export const MediaRoom = ({
 
     (async () => {
       try {
-        const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
+        const resp = await fetch(
+          `/api/livekit?room=${chatId}&username=${name}`
+        );
         const data = await resp.json();
         setToken(data.token);
       } catch (e) {
         console.log(e);
       }
-    })()
+    })();
   }, [user?.firstName, user?.lastName, chatId]);
 
   if (token === "") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Loader2
-          className="h-7 w-7 text-zinc-500 animate-spin my-4"
-        />
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">
-          Loading...
-        </p>
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
       </div>
-    )
+    );
   }
 
   return (
@@ -60,5 +55,5 @@ export const MediaRoom = ({
     >
       <VideoConference />
     </LiveKitRoom>
-  )
-}
+  );
+};
