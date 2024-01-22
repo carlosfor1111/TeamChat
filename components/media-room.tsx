1fe46b44928bb126bf0a0a14de45a07434f34f1e
carloslin <carloslin@phalanxbiotech.com>
2024-01-22 16:00:26 +0800

    media room

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,43 +1,52 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { LiveKitRoom, VideoConference } from "@liveKit/components-react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
-import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
-import { Loader2, Video } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
 interface MediaRoomProps {
   chatId: string;
   video: boolean;
   audio: boolean;
-}
+};
 
-export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
+export const MediaRoom = ({
+  chatId,
+  video,
+  audio
+}: MediaRoomProps) => {
   const { user } = useUser();
   const [token, setToken] = useState("");
+
   useEffect(() => {
     if (!user?.firstName || !user?.lastName) return;
+
     const name = `${user.firstName} ${user.lastName}`;
 
     (async () => {
       try {
-        const resp = await fetch(`/api/livekit?room${chatId}&username=${name}`);
+        const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
         const data = await resp.json();
         setToken(data.token);
-      } catch (error) {
-        console.log(error);
+      } catch (e) {
+        console.log(e);
       }
-    })();
+    })()
   }, [user?.firstName, user?.lastName, chatId]);
 
   if (token === "") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
-        <p className="text-xs text-zinc-500 dark:text-zinc-400"></p>
+        <Loader2
+          className="h-7 w-7 text-zinc-500 animate-spin my-4"
+        />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading...
+        </p>
       </div>
-    );
+    )
   }
 
   return (
@@ -51,5 +60,5 @@ export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
     >
       <VideoConference />
     </LiveKitRoom>
-  );
-};
+  )
+}
