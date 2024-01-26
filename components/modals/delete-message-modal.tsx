87a458b0dbc207b987b00829b368eb0588b5d97f
carloslin <carloslin@phalanxbiotech.com>
2024-01-26 17:27:26 +0800

    .

---
+++ components/modals/delete-message-modal.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import qs from "query-string";
+import axios from "axios";
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
